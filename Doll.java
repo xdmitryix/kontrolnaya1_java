f142ad64795b9f170201bff1bdf5acd3347bc204
dmitry <rbdeman@yandex.ru>
2023-06-12 19:28:19 +0300

    тест1

--- Doll.java
+++ Doll.java
@@ -2,6 +2,7 @@ public class Doll extends Toy {
 
     public Doll(int id, String name, int weight) {
         super(id, name, weight);
+        
     }
     
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Machine_of_toy machine_of_toy = new Machine_of_toy();
+        machine_of_toy.addToy(new Doll(1, "doll1", 20));
+        machine_of_toy.addToy(new Robot(2, "robot1", 20));
+        machine_of_toy.addToy(new Constructor(1, "constructor1", 60));
+        machine_of_toy.showAll();
     }
     
 }
