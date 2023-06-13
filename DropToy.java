bdc67f1485409930a73500892ff1496e8fbe1855
dmitry <rbdeman@yandex.ru>
2023-06-13 21:59:58 +0300

    Добавил массив для выпавших игрушек

---
+++ DropToy.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DropToy {
+        public List<Toy> toysToDrop = new ArrayList<>();
+
+    public void addToyDrop (Toy newToy){
+        this.toysToDrop.add(newToy);
+    }
+
+    public void showAllDrop(){
+        for (Toy toysToDrop: this.toysToDrop){
+            System.out.println(toysToDrop);
+        }
+    }
+}
